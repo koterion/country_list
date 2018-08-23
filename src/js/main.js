b01be3f2cc0422cda6397846bdbe1fc99dc6c9b2
Ihor <koterion317@gmai.com>
2018-08-23 14:19:49 +0300

    Rewrite for es6 standard

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-!(function( $ ) {
-    $('.phone').cntr();
-    $('.flags').cntr({flags:true});
-    $('.flag').cntr({flag:true});
-    $('.select').cntr({select:true});
-    $('.search').cntr({search:true});
-    $('.choose').cntr({select:true,list:true,countryAll:['ar','fr']});
-    $('.delete').cntr({select:true,delete:true,countryAll:['ar','ua']});
-})( jQuery );
+!(function ($) {
+  $('.phone').cntr()
+  $('.flags').cntr({flagInSelect: true})
+  $('.flag').cntr({flagInInput: true})
+  $('.select').cntr({select: true})
+  $('.search').cntr({search: true})
+  $('.choose').cntr({select: true, list: true, countryAll: ['ar', 'fr']})
+  $('.delete').cntr({select: true, delete: true, countryAll: ['ar', 'ua']})
+})(jQuery)
