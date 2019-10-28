9104471e8df9fd4032c44ac6860240fb43b898b4
koterion <koterion317@gmail.com>
2019-10-28 22:28:10 +0200

    Added new config & standard

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,18 +4,19 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
 mix.js('src/js/countryList.js', 'dist')
   .react('src/js/main.js', 'dist/js')
+  .react('src/react/countryList.js', 'dist/js')
   .sass('src/sass/countryList.sass', 'dist/css')
   .sass('src/sass/main.sass', 'dist/css')
 
-  .webpackConfig({
-    plugins: [
-      new BrowserSyncPlugin({
-        host: 'localhost',
-        port: 3000,
-        server: { baseDir: ['./'] }
-      })
-    ]
-  })
+mix.webpackConfig({
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['./'] }
+    })
+  ],
+})
 
   .options({
     processCssUrls: false,
@@ -27,4 +28,24 @@ mix.js('src/js/countryList.js', 'dist')
 if (mix.inProduction()) {
   mix.copy('*.html', '../demo/countryList')
     .copyDirectory('dist', '../demo/countryList/dist')
+
+  mix.webpackConfig({
+    externals: {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      },
+      'prop-types': {
+        root: 'PropTypes',
+        commonjs: 'prop-types',
+        commonjs2: 'prop-types',
+        amd: 'prop-types'
+      }
+    },
+    output: {
+      libraryTarget: 'umd'
+    }
+  })
 }
